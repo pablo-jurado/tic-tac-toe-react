09832228afbba633913eeed609e6dd242fd7bfae
Pablo Jurado <pjurado85@gmail.com>
2017-05-30 14:43:44 -0500

    move state to board componenent and add click handler to squares btn

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={ () => this.props.click() }>  { /* <-------  */ }
+        {this.props.val}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+   const squares = this.state.squares.slice()
+   squares[i] = 'X'
+   this.setState({squares: squares})
+   console.log(this.state.squares)
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        val={ this.state.squares[i] }
+        click={ () => this.handleClick(i) }
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+export default App;
